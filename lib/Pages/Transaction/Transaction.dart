29a8f205d1ed5a1f39f7851fab945c0216760ff4
RJ2607 <rishijain2607@gmail.com>
2024-02-11 03:23:11 +0530

    Refactor transaction page UI

--- lib/Pages/Transaction/Transaction.dart
+++ lib/Pages/Transaction/Transaction.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:ui';
+
 import 'package:finance_manager/Pages/Transaction/Expenses_exten.dart';
 import 'package:finance_manager/Pages/Transaction/Income_exten.dart';
 import 'package:flutter/cupertino.dart';
@@ -13,7 +15,6 @@ class Transaction extends StatefulWidget {
 }
 
 class _TransactionState extends State<Transaction> {
-  @override
   bool isSwitched = true;
   @override
   Widget build(BuildContext context) {
@@ -58,77 +59,108 @@ class _TransactionState extends State<Transaction> {
       height: 60,
       width: double.infinity,
       decoration: BoxDecoration(
+        // image: DecorationImage(
+        //   image: AssetImage('assets/images/me.jpg'),
+        //   fit: BoxFit.cover,
+        // ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.3),
+            spreadRadius: -5,
+            blurRadius: 25,
+          ),
+        ],
+        shape: BoxShape.rectangle,
         color: const Color.fromARGB(255, 0, 0, 0),
-        borderRadius: BorderRadius.circular(15),
       ),
-      child: Padding(
-        padding: const EdgeInsets.only(left: 5, right: 5),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              width: 160,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  gradient: LinearGradient(
-                      colors: isSwitched
-                          ? [
-                              Colors.blue,
-                              Colors.purple,
-                              Colors.pink,
-                              Colors.red
-                            ]
-                          : [Colors.black, Colors.black],
-                      transform: GradientRotation(0.5))),
-              child: TextButton(
-                onPressed: () {
-                  // create a system when pressing button then only gradient background comes
-                  setState(() {
-                    isSwitched = true;
-                  });
-                },
-                child: Text(
-                  'Income',
-                  style: TextStyle(
-                    fontSize: 17,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.white,
+      child: ClipRRect(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [Colors.white30, Colors.white10]),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 5, right: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 160,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        gradient: LinearGradient(
+                            colors: isSwitched
+                                ? [
+                                    Colors.blue,
+                                    Colors.purple,
+                                    Colors.pink,
+                                    Colors.red
+                                  ]
+                                : [
+                                    Colors.black.withOpacity(0.7),
+                                    Colors.black.withOpacity(0.5)
+                                  ],
+                            transform: GradientRotation(0.5))),
+                    child: TextButton(
+                      onPressed: () {
+                        // create a system when pressing button then only gradient background comes
+                        setState(() {
+                          isSwitched = true;
+                        });
+                      },
+                      child: Text(
+                        'Income',
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
                   ),
-                ),
+                  Container(
+                    width: 160,
+                    // padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        gradient: LinearGradient(
+                            colors: isSwitched
+                                ? [
+                                    Colors.black.withOpacity(0.7),
+                                    Colors.black.withOpacity(0.5)
+                                  ]
+                                : [
+                                    Colors.blue,
+                                    Colors.purple,
+                                    Colors.pink,
+                                    Colors.red
+                                  ],
+                            transform: GradientRotation(0.5))),
+                    child: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          isSwitched = false;
+                        });
+                      },
+                      child: Text(
+                        'Expenses',
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            Container(
-              width: 160,
-              // padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  gradient: LinearGradient(
-                      colors: isSwitched
-                          ? [Colors.black, Colors.black]
-                          : [
-                              Colors.blue,
-                              Colors.purple,
-                              Colors.pink,
-                              Colors.red
-                            ],
-                      transform: GradientRotation(0.5))),
-              child: TextButton(
-                onPressed: () {
-                  setState(() {
-                    isSwitched = false;
-                  });
-                },
-                child: Text(
-                  'Expenses',
-                  style: TextStyle(
-                    fontSize: 17,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
