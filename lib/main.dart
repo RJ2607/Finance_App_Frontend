7b0f17f990baba950a7beb5b80614723a8276de5
RJ2607 <rishijain2607@gmail.com>
2024-02-12 22:43:00 +0530

    Refactor MyApp to use StatefulWidget and add login functionality

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,27 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool login = false;
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(fontFamily: 'Raleway', brightness: Brightness.dark),
-        home: GettingStart());
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Raleway', brightness: Brightness.dark),
+      initialRoute: login ? '/home' : '/gettingStart',
+      routes: {
+        '/gettingStart': (context) => const GettingStart(),
+        '/home': (context) => const MyApp(),
+      },
+    );
   }
 }
