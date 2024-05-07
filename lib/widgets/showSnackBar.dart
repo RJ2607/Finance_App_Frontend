9843727aaf244d694f95e26f71a5181f85acce2e
RJ2607 <rishijain2607@gmail.com>
2024-05-07 15:15:42 +0530

    chore: Update showSnackBar to accept custom text color

--- lib/widgets/showSnackBar.dart
+++ lib/widgets/showSnackBar.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-showSnackBar(BuildContext context, String message) {
+showSnackBar(BuildContext context, String message, Color _color) {
   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
     shape: RoundedRectangleBorder(
       borderRadius: BorderRadius.circular(10),
     ),
     content: Text(
       message,
-      style: TextStyle(color: Colors.red, fontSize: 15),
+      style: TextStyle(color: _color, fontSize: 15),
     ),
   ));
 }
